eb29140971ae2dcb8142aa4967d2e8a76c8854c1
Softo <44883741+Kizaru1305@users.noreply.github.com>
2024-03-15 12:09:54 -0400

    added sum functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
@@ -18,29 +18,88 @@ function divide(a, b) {
 function operate(a, b, operator) {
     switch (operator) {
         case '+':
-            add(a, b);
+            result = add(a, b);
+            console.log("This is the result: " + result);
+            console.log("num1 is : " + num1);
+            console.log("num2 is : " + num2);
+            display.textContent = result;
+            return result;
         case '-':
-            subtract(a, b);
+            result = subtract(a, b);
+            display.textContent = result;
+            return result;
         case '*':
-            multiply(a, b);
+            result = multiply(a, b);
+            display.textContent = result;
+            return result;
         case '/':
-            divide(a, b);
+            result = divide(a, b);
+            display.textContent = result;
+            return result;
         default:
-            throw new Error('Invalid operator');
+            return ('Invalid operator');
     }
 }
 
 const buttons = document.querySelectorAll('.btn-num');
+const operatorBtn = document.querySelectorAll('.btn-operator');
+const clearBtn = document.querySelector('.btn-clear');
 const display = document.querySelector('.display');
+const btnEqual = document.querySelector('.btn-equal');
+
+// stores the numbers
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+let operator = '';
+
+// clears the calculator
+clearBtn.addEventListener("click", () => {
+    display.textContent = 0;
+    num1, num2 = 0;
+    operator = '';
+})
 
 
 
+
+
+// adds number to the display
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if(display.textContent == 0) {
             display.textContent = '';
+        } else if(operator) {
+            display.textContent = '';
         }
+
         display.textContent += button.textContent;
+        const number1 = display.textContent;
+        console.log(number1);
     })
 })
 
+
+operatorBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        let operation = button.textContent;
+        console.log(num1 + 's');
+        switch (operation) {
+            case '+':
+                num1 = +display.textContent;
+                if (num1 > 0) {
+                    num2 = display.textContent;
+                }
+                operator = '+';
+                break;
+            case '=':
+                
+        }
+    })
+})
+
+
+btnEqual.addEventListener("click", () => {
+    console.log("Pressed Equal");
+    operate(num1, num2, operator);
+})
