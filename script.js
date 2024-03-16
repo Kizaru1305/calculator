e26f246815f6ade67b755649951274607d84ff42
Softo <44883741+Kizaru1305@users.noreply.github.com>
2024-03-16 18:43:36 -0400

    fixed btn-operator display reset, finished project

--- script.js
+++ script.js
@@ -1,105 +1,111 @@
 function add(a, b) {
-    return +a + +b;
+  return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
-
 
 function operate(a, b, operator) {
-    switch (operator) {
-        case '+':
-            result = add(a, b);
-            console.log("This is the result: " + result);
-            console.log("num1 is : " + num1);
-            console.log("num2 is : " + num2);
-            display.textContent = result;
-            return result;
-        case '-':
-            result = subtract(a, b);
-            display.textContent = result;
-            return result;
-        case '*':
-            result = multiply(a, b);
-            display.textContent = result;
-            return result;
-        case '/':
-            result = divide(a, b);
-            display.textContent = result;
-            return result;
-        default:
-            return ('Invalid operator');
-    }
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      display.textContent = result;
+      return result;
+    case "-":
+      result = subtract(a, b);
+      display.textContent = result;
+      return result;
+    case "*":
+      result = multiply(a, b);
+      display.textContent = result;
+      return result;
+    case "/":
+      parseFloat(a);
+      parseFloat(b);
+      result = divide(a, b);
+      if (result == Infinity) {
+        alert("Error");
+        num1 = 0;
+        num2 = 0;
+      } else {
+        display.textContent = result;
+        return result;
+      }
+
+    default:
+      return "Invalid operator";
+  }
 }
 
-const buttons = document.querySelectorAll('.btn-num');
-const operatorBtn = document.querySelectorAll('.btn-operator');
-const clearBtn = document.querySelector('.btn-clear');
-const display = document.querySelector('.display');
-const btnEqual = document.querySelector('.btn-equal');
+const buttons = document.querySelectorAll(".btn-num");
+const operatorBtn = document.querySelectorAll(".btn-operator");
+const clearBtn = document.querySelector(".btn-clear");
+const display = document.querySelector(".display");
+const btnEqual = document.querySelector(".btn-equal");
 
 // stores the numbers
 let num1 = 0;
 let num2 = 0;
 let result = 0;
-let operator = '';
+
+// operator +, *, -, /
+let operator = false;
+
+// resets display of 1st number after clicking operator
+let reset = false;
 
 // clears the calculator
 clearBtn.addEventListener("click", () => {
-    display.textContent = 0;
-    num1, num2 = 0;
-    operator = '';
-})
-
-
-
-
+  display.textContent = 0;
+  num1, (num2 = 0);
+  operator = "";
+});
 
 // adds number to the display
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        if(display.textContent == 0) {
-            display.textContent = '';
-        } else if(operator) {
-            display.textContent = '';
-        }
-
-        display.textContent += button.textContent;
-        const number1 = display.textContent;
-        console.log(number1);
-    })
-})
-
+  button.addEventListener("click", () => {
+    if (display.textContent == 0) {
+      display.textContent = "";
+    } else if (num1 && reset) {
+      display.textContent = "";
+      reset = false;
+    }
+    if (display.textContent.length < 9) {
+      display.textContent += button.textContent;
+    }
+  });
+});
 
 operatorBtn.forEach((button) => {
-    button.addEventListener("click", () => {
-        let operation = button.textContent;
-        console.log(num1 + 's');
-        switch (operation) {
-            case '+':
-                num1 = +display.textContent;
-                if (num1 > 0) {
-                    num2 = display.textContent;
-                }
-                operator = '+';
-                break;
-            case '=':
-                
-        }
-    })
-})
-
+  button.addEventListener("click", () => {
+    if (operator && num1) {
+      num2 = display.textContent;
+      operate(num1, num2, operator);
+    }
+    operator = button.textContent;
+    console.log(operator);
+    if (operator === "X") {
+      operator = "*";
+    }
+    num1 = display.textContent;
+    reset = true;
+  });
+});
 
 btnEqual.addEventListener("click", () => {
-    console.log("Pressed Equal");
-    operate(num1, num2, operator);
-})
+  if (!num2 || (num2 && operator)) {
+    num2 = +display.textContent;
+  }
+
+  console.log(num1, num2);
+  result = operate(num1, num2, operator);
+});
